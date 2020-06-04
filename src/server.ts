bf6d765af3b7f4ccf0c71007c2f029a2c5b5416d
Mateus Nadal <nlmatt07@hotmail.com>
2020-06-04 13:57:25 -0300

    App Base

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  console.log("listagem de usuarios");
+
+  response.json(["Diego", "Mateus", "Mateus", "Eu"]);
+});
+
+app.listen(3333);
